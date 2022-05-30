31f25612bb7d30a0d9a20acbb16489c705361925
007Chanak <70619452+007Chanak@users.noreply.github.com>
2022-05-30 19:57:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import {createSwitchNavigator, createAppContainer} from "react-navigation";
+import {CreateTask} from './CreateTask';
+import {Calender} from "./Calender";
+import {DisplayTask} from "./DisplayTask";
+import {Front} from "./Front";
+import {Home} from "./Home";
+import {Intro} from "./Intro";
